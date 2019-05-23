9682036176617ad8a4e6deddc2fe85c02fff7082
demo07 <dddemesvar07@gmail.com>
2019-05-23 22:19:50 +0000

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.action_mailer.asset_host = 'https://myrates.herokuapp.com'
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
