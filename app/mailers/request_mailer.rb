f81592b9f8ff1a7c46137b08f35f21734991cf49
demo07 <dddemesvar07@gmail.com>
2019-05-25 03:27:23 +0000

    commit

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,6 +1,7 @@
 class RequestMailer < ApplicationMailer
     add_template_helper(ApplicationHelper)
-    default from: 'MyRates', sender: 'no-reply@myrates.co', reply_to: 'help@myrates.co'
+    default from: %("MyRates" <"no-reply@myrates.co">)
+    default sender: 'no-reply@myrates.co', reply_to: 'help@myrates.co'
  
     def send_request(promo_request)
         @request = promo_request
